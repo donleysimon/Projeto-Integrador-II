394480d6cbfd399620d9123c652e7bfdd602a8e0
Peter Simon <donleysimon@gmail.com>
2018-04-05 19:52:21 -0300

    limpeza do codigo e remoção da casa

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import color
 import characters
 import objects
 pygame.init()
-screen = pygame.display.set_mode((600, 400))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("PyGame")
 Dia = True
 pygame.mouse.set_visible(False)
@@ -12,26 +12,6 @@ player = pygame.surface.Surface((15, 30))
 jogoAtivo = True
 while jogoAtivo:
     screen.fill(color.blueSkyLight)
-
-    bloco = pygame.Rect(200, 200, 150, 150)
-    porta = pygame.Rect(220,300,25,50)
-    janela = pygame.Rect(270, 310, 25, 25)
-    janela2 = pygame.Rect(310, 310, 25, 25)
-    janela3 = pygame.Rect(310, 230, 25, 25)
-    janela4 = pygame.Rect(270, 230, 25, 25)
-    janela5 = pygame.Rect(220, 230, 25, 25)
-    pygame.draw.rect(screen, (200, 200, 200), bloco)
-    pygame.draw.rect(screen,(10,10,10),porta)
-    pygame.draw.rect(screen,(255,255,255),janela)
-    pygame.draw.rect(screen, (255, 255, 255), janela2)
-    pygame.draw.rect(screen, (255, 255, 255), janela3)
-    pygame.draw.rect(screen, (255, 255, 255), janela4)
-    pygame.draw.rect(screen, (255, 255, 255), janela5)
-
-
-
-    pygame.draw.polygon(screen,(150,150,150),[(200,200),(275,150),(350,200)],0)
-
 
 
 
@@ -61,8 +41,8 @@ while jogoAtivo:
     player.set_colorkey(color.colorKey)
     player2.fill(color.colorKey)
     player2.set_colorkey(color.colorKey)
-    bla = characters.Skier()
-    characters.Skier.down(bla, player, 0, 0, 1)
+    skier = characters.Skier()
+    characters.Skier.down(skier, player, 0, 0)
     pygame.transform.scale(player, (45, 90), player2)
     screen.blit(player2, (xMouse, yMouse))
     pygame.display.flip()
