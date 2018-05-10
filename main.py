224f36144457eb4cd500ee57733dfc59b97efeda
igulino <38089358+igulino@users.noreply.github.com>
2018-05-10 13:53:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -28,6 +28,45 @@ totalArvores = 5
 listaArvores = pygame.sprite.Group()
 listaTotal = pygame.sprite.Group()
 
+class HitBox(pygame.sprite.Sprite):
+    '''This class represents the ball
+    It derives from the "Sprite" class in Pygame
+    '''
+
+
+    def __init__(self, color, width, height):
+        """ Constructor. Pass in the color of the block,
+        and its x and y position. """
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([width, height])
+        #self.image.fill(color)
+        skier = characters.Skier()
+        self.image.set_colorkey((1,1,1))
+        characters.Skier.playerDown(skier, self.image, 0, 0)
+
+
+        # Fetch the rectangle object that has the dimensions of the image
+        # image.
+        # Update the position of this object by setting the values
+        # of rect.x and rect.y
+        self.rect = self.image.get_rect()
+
+
+    def update(self,x, y):
+        """ Called each frame. """
+
+        # Move block down one pixel
+        self.rect.x = x
+        self.rect.y = y
+
+hitBox_list = pygame.sprite.Group()
+hitBox_Player = HitBox((1,1,1),34,62)
+hitBox_list.add(hitBox_Player)
+
 for i in range(totalArvores):
     tree = objects.Tree(color.brown, 20, 20, velocidade, larguraTela)
 
@@ -58,6 +97,7 @@ while jogoAtivo:
                     Dia = True
     screen.fill(color.snow)
 
+    hitBox_list.draw(screen)
     #Criacao de Personagem (fundo e personagem)
     player = pygame.Surface((34, 62))
     #player2 = pygame.Surface((42, 60))
@@ -68,13 +108,16 @@ while jogoAtivo:
     #player2.fill(color.colorKey)
     #player2.set_colorkey(color.colorKey)
 
-
+    hitBox_list.update(posX - 17, posY - 31)
     skier = characters.Skier()
     #characters.Skier.down(skier, player, 0, 0)
     characters.Skier.playerDown(skier, player, 0, 0)
     #pygame.transform.scale(player, (42, 60), player2)
-    screen.blit(player, (posX - 17, posY - 31))
+    #screen.blit(player, (posX - 17, posY - 31))
 
+    blocks_hit_list = pygame.sprite.spritecollide(hitBox_Player, listaTotal, False)
+    for block in blocks_hit_list:
+        velocidade = 0
 
     # Declara e atualiza posicao do mouse
     posMouse = pygame.mouse.get_pos()
